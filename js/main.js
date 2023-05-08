61be0965849eb54f6508f2d87432ad6598b20a9b
Alex <cabezacarsialejandro@gmail.com>
2023-05-08 17:12:39 +0200

    Práctica acabada

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const PI = 3.141592; 
+let radio = prompt ('Introduce el radio de tu círculo ');
+
+radio = parseFloat(radio) 
+
+const AREACIRCULO = PI * (radio*radio);
+
+alert ('El área de tu círculo es: ' + AREACIRCULO)
+
